be758a55b6b42bbfac919a454655456aa12509e2
Elan Taseen <elantaseen@gmail.com>
2024-05-06 22:34:46 +0600

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Homepage from "./page";
 
 
 const inter = Inter({ subsets: ["latin"] });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,121 @@
-import Image from "next/image";
-const Homepage = () => {
-  return 
-  <div className="h-full flex flex-col">
-    {/* image container*/}
-    <div className="h-1/2 relative bg-red-200">
-      <Image src="/hero.png" alt='' className="object-contain"/>
-    </div>
+// "use client"
+// import Image from "next/image";
+// const Homepage = () => {
+//   return 
+//   <div className="h-full flex flex-col gsp-8 items-crnter justify-center">
+// {/* image container */}
+//     <div className="h-1/2 relative">
+//       <Image src="/hero.png" alt='' className="object-contain"/> <Image src="/hero.png" alt="" fill className="object-contain" />
+//     </div>
+//     {/* text-container */}
+//   <div className="h-1/2">
+//     {/* title */}
+//     <h1>Crafting Digital Experience</h1>
+//     <p>
+//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, architecto quas repellendus veritatis assumenda reiciendis, doloremque velit consectetur amet quia dolores ad ea dolorum quos alias reprehenderit pariatur illum! Harum!
+//     </p>
+//     {/* button */}
+//     <div>
+//       <button>View My Work</button>
+//       <button>View My Work</button>
+//     </div>
+//     </div>;
+//   </div>
+// };
+
+// export default Homepage;
+
+
+// saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+// "use client";
+
+// import Image from "next/image";
+
+// const Homepage = () => {
+//   return (
     
-  <div className="h-1/2">
-    </div>;
-  </div>
+//       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+//         {/* IMAGE CONTAINER */}
+//         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+//           <Image src="/hero.png" alt="" fill className="object-contain" />
+//         </div>
+//         {/* TEXT CONTAINER */}
+//         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+//           {/* TITLE */}
+//           <h1 className="text-4xl md:text-6xl font-bold">
+//             Crafting Digital Experiences, Designing Tomorrow.
+//           </h1>
+//           {/* DESC */}
+//           <p className="md:text-xl">
+//             Welcome to my digital canvas, where innovation and creativity
+//             converge. With a keen eye for aesthetics and a mastery of code, my
+//             portfolio showcases a diverse collection of projects that reflect my
+//             commitment to excellence.
+//           </p>
+//           {/* BUTTONS */}
+//           <div className="w-full flex gap-4">
+//             <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+//               View My Work
+//             </button>
+//             <button className="p-4 rounded-lg ring-1 ring-black">
+//               Contact Me
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+    
+//   );
+// };
+
+// export default Homepage;
+
+
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/taseen.jpeg" alt="" fill className="object-contain " />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Crafting Digital Experiences, Designing Tomorrow.
+          </h1>
+          {/* DESC */}
+          <p className="md:text-xl">
+            Welcome to my digital canvas, where innovation and creativity
+            converge. With a keen eye for aesthetics and a mastery of code, my
+            portfolio showcases a diverse collection of projects that reflect my
+            commitment to excellence.
+          </p>
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Homepage;
